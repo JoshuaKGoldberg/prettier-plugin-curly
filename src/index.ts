1c56e0d212924fb8b14ce4616e91a07581de1d94
fisker <lionkay@gmail.com>
2025-10-14 06:11:17 +0800

    Clean

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ We need expose parsers here
 See https://github.com/prettier/prettier/pull/18072
 
 After we drop support for Prettier<v3.7.0
-We can remove all exports except the parser
+We can remove all exports except the `printers`
 */
 import * as babel from "prettier/parser-babel";
 import * as typescript from "prettier/parser-typescript";
